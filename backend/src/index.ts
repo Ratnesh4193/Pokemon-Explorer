207a2d6e36175bdfc426d0577babd2e6f51aba49
Ratnesh Kumar Tiwari <ratnesh@Ratneshs-MacBook-Air.local>
2025-11-01 23:04:52 +0530

    added vercel configs for deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import { config } from './config/app.config';
 import healthRoutes from './routes/health.routes';
 import pokemonRoutes from './routes/pokemon.routes';
 
@@ -13,8 +12,3 @@ app.use(express.json());
 // Routes
 app.use('/ping', healthRoutes);
 app.use('/api/pokemon', pokemonRoutes);
-
-// Start server
-app.listen(config.port, () => {
-  console.log(`🚀 Server running on http://localhost:${config.port}`);
-});
